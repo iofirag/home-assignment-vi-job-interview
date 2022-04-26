3a49f9d6dc421f2441c081e71ea9ec10555d17a9
iofirag <iofirag@gmail.com>
2022-04-26 20:13:35 +0300

    ddd

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -2,12 +2,8 @@ const container = require("../containerConfig");
 
 const movieService = container.resolve("movieService");
 
-// exports = {
-//     listMoviesByActors: movieService.listMoviesByActors.bind(movieService),
-//     listMultipleCharactersActors: movieService.listMultipleCharactersActors.bind(movieService),
-//     getSameRoleActors: movieService.getSameRoleActors.bind(movieService),
-// };
-
-exports.listMoviesByActors = movieService.listMoviesByActors.bind(movieService);
-exports.listMultipleCharactersActors = movieService.listMultipleCharactersActors.bind(movieService);
-exports.getSameRoleActors = movieService.getSameRoleActors.bind(movieService);
+module.exports = {
+    listMoviesByActors: movieService.listMoviesByActors.bind(movieService),
+    listMultipleCharactersActors: movieService.listMultipleCharactersActors.bind(movieService),
+    getSameRoleActors: movieService.getSameRoleActors.bind(movieService),
+};
